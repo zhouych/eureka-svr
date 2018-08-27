78850632b3955db0809d8e6858b0095b259e6ebb
zhouych <zhouych329@163.com>
2018-08-27 18:45:33 +0800

    ...

---
+++ src/main/java/com/zyc/eureka/svr/EurekaSvrApplication.java
@@ -0,0 +1,15 @@
+package com.zyc.eureka.svr;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaSvrApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaSvrApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+}
---
+++ src/test/java/com/zyc/eureka/svr/EurekaSvrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyc.eureka.svr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaSvrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
