2db7e7aa45ae2c339d08b9e588d48f5a6f9c4ff6
zhouych <zhouych329@163.com>
2018-09-12 18:16:43 +0800

    ...

--- src/main/java/com/zyc/eureka/svr/EurekaSvrApplication.java
+++ src/main/java/com/zyc/eureka/svr/EurekaSvrApplication.java
@@ -3,6 +3,7 @@ package com.zyc.eureka.svr;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
